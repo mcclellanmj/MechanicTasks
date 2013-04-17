d3c24ca8290d061e5eaf6fb0b0770e07fddea5ac
Matt McClellan <mcclellan.mj@gmail.com>
2013-04-17 15:56:36 -0500

    Exports all files to the target directory now More efficient at filtering directories out

--- make.py
+++ make.py
@@ -1,4 +1,5 @@
 import os
+import distutils.core
 from string import Template
 
 json_template = Template(open("template.json", "r").read())
@@ -8,13 +9,18 @@ def write(to_file, content):
 	out_file.write(content)
 	out_file.close
 
-for dir_part, dir_names, file_names in os.walk('.'):
-	if not dir_part.startswith("./.git"):
-		for dir_name in dir_names:
-			if not dir_name.startswith(".git"):
-				def mapper(filename):
-					return '"' + dir_name + "/" + filename + '"'
+if not os.path.exists("target"):
+	os.makedirs("target")
 
-				write(dir_name + ".json", json_template.substitute(
-					directory=dir_name, paths=",".join(
-					map(mapper, os.listdir(dir_name)))))
+for dir_part, dir_names, file_names in os.walk('.', topdown=True):
+	dir_names[:] = [d for d in dir_names if d not in [".git", "target"]]
+
+	for dir_name in dir_names:
+		def mapper(filename):
+			return '"' + dir_name + "/" + filename + '"'
+
+		write("target/" + dir_name + ".json", json_template.substitute(
+			directory=dir_name, paths=",".join(
+			map(mapper, os.listdir(dir_name)))))
+
+		distutils.dir_util.copy_tree(dir_name, "target/" + dir_name)
