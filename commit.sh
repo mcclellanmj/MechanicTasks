7f499359283b60996a342706c92e8cb86f3aeb8d
Matt McClellan <mcclellan.mj@gmail.com>
2013-05-28 17:05:52 -0500

    No longer commits with email

--- commit.sh
+++ commit.sh
@@ -8,6 +8,6 @@ python make.py
 git checkout gh-pages
 cp -r target/* .
 git add --all
-git commit -m "Auto-commit to gh-pages" --author="Auto Script <null@null.com>"
+git commit -m "Auto-commit to gh-pages" --author="Auto Commit <>"
 git push origin
 git checkout master
