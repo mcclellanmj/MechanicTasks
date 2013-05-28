84cde1cfacdee67543d4ce8e15bf5220ceb94b5f
Matt McClellan <mcclellan.mj@gmail.com>
2013-05-28 16:43:47 -0500

    Autoscript commits as different author

--- commit.sh
+++ commit.sh
@@ -3,6 +3,6 @@ python make.py
 git checkout gh-pages
 cp -r target/* .
 git add --all
-git commit -m "Auto-commit to gh-pages"
+git commit -m "Auto-commit to gh-pages" --author="Auto Script <null@null.com>"
 git push origin
 git checkout master
